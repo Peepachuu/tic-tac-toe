cfba1e47ad2ffa98681dc906c4767e4e9708ded1
Zafir <zafirhasan2006@gmail.com>
2022-09-05 14:56:03 +0400

    Create logic for checking the winner

--- game.js
+++ game.js
@@ -9,12 +9,40 @@ const gameBoard = (function() {
         board[posX][posY] = marker;
     }
 
+    function displayBoard() {
+        return board;
+    }
+
+    function decideVerdict() {
+    }
+
+    function checkForWinner() {
+        // check vertical
+        for (let x = 0; x < 3; ++x) {
+            if (board[x][0] == board[x][1] && board[x][1] == board[x][2])
+                return board[x][0];
+        }
+        // check horizontal
+        for (let y = 0; y < 3; ++y) {
+            if (board[0][y] == board[1][y] && board[1][y] == board[2][y])
+                return board[0][y];
+        }
+        // check diagonal
+        if (board[0][0] == board[1][1] == board[2][2])
+            return board[0][0];
+        if (board[2][0] == board[1][1] == board[0][2])
+            return board[2][0];
+    }
     return {
-        board,
-        insertMarker
+        insertMarker,
+        displayBoard,
+        checkForWinner
     }
 })();
 
-console.log(gameBoard.board);
-gameBoard.insertMarker('X', 0, 2);
-console.log(gameBoard.board);
+console.log(gameBoard.displayBoard());
+gameBoard.insertMarker('X', 1, 1);
+gameBoard.insertMarker('X', 0, 0);
+gameBoard.insertMarker('X', 0, 1);
+console.log(gameBoard.checkForWinner());
+console.log(gameBoard.displayBoard());
